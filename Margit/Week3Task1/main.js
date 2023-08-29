b0de2f69c850c25ca8a5d3d6a298dcad29fad8f3
katiperkio <perkiokati@outlook.com>
2023-08-29 14:21:21 +0300

    added condition for NaN

--- Margit/Week3Task1/main.js
+++ Margit/Week3Task1/main.js
@@ -3,10 +3,12 @@ const price = document.querySelector('#price').value
 const money = document.querySelector('#money').value
 const answer = document.querySelector('#answer')
 
-const amount = (money/price).toFixed(3);
+const amount = (money/price).toFixed(2);
 
 if (amount >= 10) {
     answer.textContent = `Oh, it's a sweet escape! You can get ${amount} liters with your money.`
+} else if (amount === "NaN") {
+    answer.textContent= `Oops, I guess you're stuck here! You can get 0.00 liters with your money.`
 } else {
     answer.textContent = `Oops, I guess you're stuck here! You can get ${amount} liters with your money.`
 }
